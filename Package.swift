bc13be3e03df1db1239262e130135ff6f6dfb949
Сергеев Эдуард Олегович <sergeeveo@akbarsdigital.ru>
2024-10-11 16:32:20 +0700

    ~ попытка фикса SPM

--- Package.swift
+++ Package.swift
@@ -12,7 +12,7 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://gitlab.akbars.tech/abo/ios-service-network", .upToNextMajor(from: "1.0.0")),
-        .package(url: "https://gitlab.akbars.tech/abo/ios-extensions", .upToNextMajor(from: "0.1.0")),
+        .package(url: "https://gitlab.akbars.tech/abo/ios-extensions.git", .upToNextMajor(from: "0.1.0")),
         .package(url: "https://gitlab.akbars.tech/abo/ios-services", .upToNextMajor(from: "0.3.0")),
     ],
     targets: [
